ffa89ccbcae1b0f11add9a5665b160902b67b039
Peter Zhu <peterzhu2118@gmail.com>
2019-02-13 12:30:13 -0500

    Update the demo

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   devise_for :users
 
+  root to: 'demo#index'
+
   authenticate :user, ->(_u) { false } do
     mount Sidekiq::Web => '/sidekiq'
   end
--- test/integration/sidekiq_url_test.rb
+++ test/integration/sidekiq_url_test.rb
@@ -4,10 +4,12 @@ require 'test_helper'
 
 class SidekiqUrlTest < ActionDispatch::IntegrationTest
   test 'problematic test' do
-    p new_user_session_path
+    p "new_user_session_path before request: #{new_user_session_path}"
+    p "root_path before request: #{root_path}"
 
     get '/sidekiq'
 
-    p new_user_session_path
+    p "new_user_session_path after request: #{new_user_session_path}"
+    p "root_path after request: #{root_path}"
   end
 end
